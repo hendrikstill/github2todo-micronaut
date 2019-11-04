ba2b102bad6b777ec5426d5ea54f3a384e5f4e48
Hendrik Still <hendrik.still@inovex.de>
2019-11-04 17:30:47 +0100

    feat:upgrade to new todo-app version

--- src/main/kotlin/github2todo/micronaut/TodoAppClient.kt
+++ src/main/kotlin/github2todo/micronaut/TodoAppClient.kt
@@ -1,12 +1,12 @@
 package github2todo.micronaut
 
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.annotation.Client
 import io.reactivex.Single
 
 @Client("http://todo-app-web.todo-app.knative-meetup-poc.still.network")
 interface TodoAppClient {
-    @Get("/insert/todo/{todo}")
+    @Post("/insert/todo/{todo}")
     fun createNewTask(todo: String): Single<HttpResponse<String>>
 }
